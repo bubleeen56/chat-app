50f936150732a1fb642209c8cbe7aecd0a3ab7a2
bubleeen56 <151603995+bubleeen56@users.noreply.github.com>
2023-12-04 14:48:55 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index" #ルートパスへのアクセスで　indexアクション呼び出す
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
