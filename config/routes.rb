2574f2e17f071b2d2c0e63a050601b72df15c8bf
bubleeen56 <151603995+bubleeen56@users.noreply.github.com>
2023-12-04 11:40:22 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "messages#index" #ルートパスへのアクセスで　indexアクション呼び出す
+  resources :users, only: [:edit, :update]
 end
